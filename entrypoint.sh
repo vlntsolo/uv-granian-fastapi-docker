b7347484c365385dd43ac30aa11875023934feb8
Valentyn Solonechnyi <valentinesolo@gmail.com>
2024-10-27 12:52:02 +0100

    init

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Start granian..."
+# granian {{ project_name }}.wsgi:application \
+#     --host 0.0.0.0 \
+#     --port 8000 \
+#     --interface wsgi \
+#     --no-ws \
+#     --loop uvloop \
+#     --process-name \
+#     "granian [{{ project_name }}]"
+granian --port 8000 --interface asgi --host 0.0.0.0 hello:app --process-name "fastapi with granian"
